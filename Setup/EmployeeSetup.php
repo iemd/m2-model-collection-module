940d3880425e07241ffb313ad32845256cb90063
Omprakash Thakur <iewd1285@gmail.com>
2021-11-25 01:23:15 +0530

    created simple and eav model

---
+++ Setup/EmployeeSetup.php
@@ -0,0 +1,38 @@
+<?php
+/*
+* Magento creates attributes and registers a new entity type
+* in eav_attributes and eav_entity_type tables.
+*/
+
+namespace Foggyline\Office\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+
+class EmployeeSetup extends EavSetup
+{
+    public function getDefaultEntities()
+    {
+        $employeeEntity = \Foggyline\Office\Model\Employee::ENTITY;
+        $entities = [
+            $employeeEntity => [
+                'entity_model' => 'Foggyline\Office\Model\ResourceModel\Employee',
+                'table' => $employeeEntity . '_entity',
+                'attributes' => [
+                    'department_id' => [
+                        'type' => 'static',
+                    ],
+                    'email' => [
+                        'type' => 'static',
+                    ],
+                    'first_name' => [
+                        'type' => 'static',
+                    ],
+                    'last_name' => [
+                        'type' => 'static',
+                    ],
+                ],
+            ],
+        ];
+        return $entities;
+    }
+}
