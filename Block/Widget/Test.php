a9e3788901a49338ddf99b324c6016e7941c8ed3
Omprakash Thakur <iewd1285@gmail.com>
2022-02-26 15:42:45 +0530

    using widgets

---
+++ Block/Widget/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Foggyline\Office\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Test extends Template implements BlockInterface
+{
+    protected $_template = "widget/test.phtml";
+}
